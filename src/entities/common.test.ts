dbbe73321b5c5eca65516dc33637dbfc0ee7fd00
Ville Koskela <vkoskela@gmail.com>
2024-04-17 21:21:36 +0300

    test: simple test

--- src/entities/common.test.ts
+++ src/entities/common.test.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert";
 import { describe, it } from "node:test";
 
 import type { Database } from "../ports/database.port";
@@ -7,13 +8,16 @@ import { createEntityFactory } from "./common";
 export const name = "Entities common";
 
 void describe("Entities common", undefined, () => {
-  void it("should something", () => {
+  void it("should create person", () => {
     const foo = createEntityFactory({} as Database);
     const person = foo.createPerson();
 
     person.username = "Foo";
     person.password = "Bar";
     person.realname = "Foo Bar";
-    console.log(JSON.stringify(person, undefined, 2));
+
+    assert.equal(person.username, "Foo");
+    assert.equal(person.password, "Bar");
+    assert.equal(person.realname, "Foo Bar");
   });
 });
